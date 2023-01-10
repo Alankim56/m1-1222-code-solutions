b6ff3bb85f595aa8206f8bdc565f600ed045901b
Alan Kim <alankim0622@gmail.com>
2023-01-10 05:24:39 +0000

    completed string manipulation 1

---
+++ string-manipulation-1/src/is-vowel.js
@@ -0,0 +1,10 @@
+/* exported isVowel */
+// if char is a vowel, return true
+// if char is not a vowel, return false
+function isVowel(char) {
+  if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' || char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') {
+    return true;
+  } else {
+    return false;
+  }
+}
