6bff1f0a9efaebd35aea8fb8682947d3220bcac6
Alan Kim <alankim0622@gmail.com>
2023-03-06 04:18:17 +0000

    completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,73 @@
+var orderHistory = [
+  {
+    orderNumber: '114-3941689-8772232',
+    orderPlaced: 'August 4, 2020',
+    orderDelivered: 'August 8, 2020',
+    shipTo: 'JS Masher',
+    total: 34.00,
+    item: [
+      {
+        image: '',
+        name: 'Javascript for impatient programmers',
+        author: 'Rauschmayer, Dr.Axel',
+        price: 31.55,
+        returnBy: 'September 7, 2020'
+      }
+    ]
+  },
+  {
+    orderNumber: '113-9984268-1280257',
+    orderPlaced: 'July 19, 2020',
+    orderDelivered: 'July 20, 2020',
+    shipTo: 'JS Masher',
+    total: 44.53,
+    item: [
+      {
+        image: '',
+        name: 'The Timeless Way of Buliding',
+        author: 'Alexander, Christopher',
+        price: 41.33,
+        returnBy: 'August 19, 2020'
+      }
+    ]
+  },
+  {
+    orderNumber: '114-2875557-9059409',
+    orderPlaced: 'July 4, 2020',
+    orderDelivered: 'July 7, 2020',
+    shipTo: 'JS Masher',
+    total: 17.22,
+    item: [
+      {
+        image: '',
+        name: 'Gamecube Controller Adapter, Super Smash Bros Switch Gamecube Adaptor for WII U',
+        author: null,
+        price: 15.98,
+        returnBy: 'August 5, 2020'
+      }
+    ]
+  },
+  {
+    orderNumber: '113-2883177-2648248',
+    orderPlaced: 'July 3, 2020',
+    orderDelivered: 'July 5, 2020',
+    shipTo: 'JS Masher',
+    total: 138.93,
+    item: [
+      {
+        image: '',
+        name: 'GameCube Controller',
+        author: null,
+        price: 94.95,
+        returnBy: 'August 4, 2020'
+      },
+      {
+        image: '',
+        name: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        price: 33.99,
+        returnBy: 'August 4, 2020'
+      }
+    ]
+  }
+];
